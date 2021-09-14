a8f0d10cdd18b1cb33b146bb5dba1796622b498b
lamquocminhhuy <lamquocminhhuy@gmail.com>
2021-09-14 23:24:29 +0700

    deploy heroku

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,9 +1,17 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('webtcan', 'root', "password", {
-  host: 'localhost',
-  dialect: 'mysql',
-  logging: false
+const sequelize = new Sequelize('d2uivc17a0acdt', 'okultaptlbfcuv', '7b6487c14595de4378b22bc964b8151be8e88c844670381b6558c206f3a9032a', {
+  host: 'ec2-44-198-223-154.compute-1.amazonaws.com',
+  dialect: 'postgres',
+  logging: false,
+
+  
+    "dialectOptions": {
+      "ssl": {
+        "require": true,
+        "rejectUnauthorized": false
+      }
+    }
 });
 
 let connectDB = async () => {
