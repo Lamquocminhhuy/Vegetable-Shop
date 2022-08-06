f498bd05a9e7ea5b2f23b0c760f7e7f29f53483d
Lam Quoc Minh Huy <lamquocminhhuy@gmail.com>
2022-08-06 12:15:36 +0700

    first commit

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,22 +1,18 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
 
-
-const sequelize = new Sequelize('vegetable-shop', 'root', 'password', {
-  host: 'localhost',
-  dialect: 'mysql',
-  logging: false
-
-
+const sequelize = new Sequelize("postgres", "postgres", "Lamquocminhhuy123", {
+  host: "db.jezplnfcoamzlxqzimek.supabase.co",
+  dialect: "postgres",
+  logging: false,
 });
 
 let connectDB = async () => {
-    try {
-        await sequelize.authenticate();
-        console.log('Connection to database has been established successfully.');
-      } catch (error) {
-        console.error('Unable to connect to the database:', error);
-      }
-}
-
+  try {
+    await sequelize.authenticate();
+    console.log("Connection to database has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
 
 module.exports = connectDB;
